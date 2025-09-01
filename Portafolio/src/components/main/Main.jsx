51bc06c3909cf376f9691221e2f2f6b56c003a0c
Fernando Koss <fermkoss@gmail.com>
2025-09-01 00:14:22 -0300

    creacion del main

---
+++ Portafolio/src/components/main/Main.jsx
@@ -0,0 +1,25 @@
+import styles from "./main.module.css"
+const Main = () => {
+
+    return <main>
+        <div  >
+            <h2>Sobre Mi</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio dignissimos aliquid voluptas vero fugiat, odit minima placeat rem sunt suscipit doloribus quidem, ipsum officia id ipsam nisi maxime laudantium! Ducimus!</p>
+        </div>
+        <div>
+            <h2>Educacion</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam, consectetur? Quasi cum animi nisi amet repellendus recusandae sint, dolores ipsa dolore eius asperiores in voluptatum esse ipsum tempora sequi alias!</p>
+        </div>
+        <div>
+            <h2>Lenguajes</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vel necessitatibus alias numquam, ipsum excepturi reprehenderit, vero illum explicabo animi debitis ratione, quod eligendi quidem? Delectus laboriosam mollitia dolores molestiae nostrum?</p>
+        </div>
+        <div>
+            <h2>Experiencia Laboral</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt doloribus nam quidem eligendi minus cupiditate, molestias hic ea culpa! Nesciunt veritatis in illo reprehenderit consectetur repudiandae incidunt saepe dolor est.</p>
+        </div>
+        
+
+    </main>
+}
+export default Main

c93026bae82cedc12346a479dedaf4d36cdb1a2c
Fernando Koss <fermkoss@gmail.com>
2025-09-15 16:06:24 -0300

    funcion form

--- Portafolio/src/components/footer/Footer.jsx
+++ Portafolio/src/components/footer/Footer.jsx
@@ -1,8 +1,27 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import styles from "./footer.module.css"
 import { modeContext } from "../../context/ModeContext"
+import emailjs from '@emailjs/browser';
 const Footer = () => {
     const {mode} = useContext(modeContext)
+
+    const [name,setName]=useState("");
+    const [email,setEmail]=useState("");
+    const [message,setMessage]=useState("");
+    
+    const enviarMail = (e) =>{
+        emailjs.send("service_n2lxghc","template_l0x5k8k",{
+from_name: name,
+message: message,
+email_id: email,
+},
+"tvR12jHg4Tez_c-w3"
+);
+setEmail("")
+setName("")
+setMessage("")
+    }
+
     return (<footer style={mode.footer}>
         <div className={styles.contacto}>
             <h3 style={mode.text} >Contacto:</h3>
@@ -18,10 +37,10 @@ const Footer = () => {
         <div className={styles.escribime}>
             <h3 style={mode.text} >Enviar consulta</h3>
             <form className={styles.mandarMail}>
-                <div><label style={mode.text} htmlFor="">Nombre Completo: </label><input type="text" /></div>
-                <div><label style={mode.text} htmlFor="">Email: </label><input type="text" /></div>
-                <div><label style={mode.text} htmlFor="">Mensaje: </label><textarea name="" id=""></textarea></div>
-                <button type="button" style={mode.botonSend} >Enviar</button>
+                <div><label style={mode.text} htmlFor="">Nombre Completo: </label><input type="text" value={name} onChange={(e)=>{setName(e.target.value)}}/></div>
+                <div><label style={mode.text} htmlFor="">Email: </label><input type="text" value={email} onChange={(e)=>{setEmail(e.target.value)}}/></div>
+                <div><label style={mode.text} htmlFor="">Mensaje: </label><textarea name="" id="" value={message} onChange={(e)=>{setMessage(e.target.value)}}></textarea></div>
+                <button type="button" style={mode.botonSend} onClick={enviarMail}>Enviar</button>
             </form>
         </div>
 
